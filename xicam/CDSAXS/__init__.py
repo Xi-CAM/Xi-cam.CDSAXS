79251af1108083d05860ba85869667d38fc4f1ee
Ronald Pandolfi <ronpandolfi@gmail.com>
2018-11-20 14:34:35 -0800

    First commit, porting old xi-cam 1 plugin

---
+++ xicam/CDSAXS/__init__.py
@@ -0,0 +1,236 @@
+import os, sys, time
+from xicam.CDSAXS import simulation, fitting, cdsaxs, data_treatment, custom_widget
+import pyqtgraph as pg
+import numpy as np
+from xicam.plugins import GUIPlugin
+from xicam.gui import threads
+from modpkgs import guiinvoker
+from functools import partial
+from PySide import QtGui, QtCore
+import multiprocessing
+import deap.base as deap_base
+from deap import creator
+from scipy import interpolate
+import fabio
+
+creator.create('FitnessMin', deap_base.Fitness, weights=(-1.0,))  # want to minimize fitness
+creator.create('Individual', list, fitness=creator.FitnessMin)
+
+
+class plugin(base.plugin):
+    name = "CDSAXS"
+
+    def __init__(self, *args, **kwargs):
+
+        self.centerwidget = QtGui.QTabWidget()
+        self.datatreatmenttree = pg.parametertree.ParameterTree()
+        self.materialstree = pg.parametertree.ParameterTree()
+        self.rightmodes = [(self.datatreatmenttree, QtGui.QFileIconProvider().icon(QtGui.QFileIconProvider.Desktop)),
+                           (self.materialstree, QtGui.QFileIconProvider().icon(QtGui.QFileIconProvider.Computer))]
+        self.topwidget = QtGui.QTabWidget()
+        self.centerwidget.setDocumentMode(True)
+        self.centerwidget.setTabsClosable(True)
+        self.centerwidget.tabCloseRequested.connect(self.tabClose)
+        self.centerwidget.currentChanged.connect(self.currentChanged)
+        self.bottomwidget = custom_widget.CDLineProfileWidget()
+
+        # Setup parametertree
+
+        self.datatreatmentparam = pg.parametertree.Parameter.create(name='params', type='group', children=[
+            {'name': 'Experimental input', 'type': 'group', 'children': [
+                {'name': 'Start angle', 'type': 'float', 'value': 0},
+                {'name': 'End angle', 'type': 'float', 'value': 0},
+                {'name': 'Angle step', 'type': 'float', 'value': 0},
+                {'name': 'Line pitch', 'type': 'float', 'value': 86},
+                {'name': 'Data treatment', 'type': 'action'}]},
+            {'name': 'Initial line Profile', 'type': 'group', 'children': [
+                {'name': 'Number trapezoid', 'type': 'float', 'value': 5},
+                {'name': 'Heigth', 'type': 'float', 'value': 20},
+                {'name': 'Linewidth', 'type': 'float', 'value': 50},
+                {'name': 'Sidewall angle', 'type': 'float', 'value': 90},
+                {'name': 'Simulation', 'type': 'action'}]},
+            {'name': 'Fit output', 'type': 'group', 'children': [
+                {'name': 'num trap fit', 'type': 'float', 'readonly': True},
+                {'name': 'H fit', 'type': 'float', 'readonly': True},
+                {'name': 'w0 fit', 'type': 'float', 'readonly': True},
+                {'name': 'Beta fit', 'type': 'float', 'readonly': True},
+                {'name': 'f_val', 'type': 'float', 'readonly': True}]}])
+        self.datatreatmenttree.setParameters(self.datatreatmentparam, showTop=False)
+
+        self.materialparam = pg.parametertree.Parameter.create(name='params', type='group', children=[
+            {'name': 'Material', 'type': 'group', 'children': [
+                {'name': 'Substrate thickness', 'type': 'float', 'value': 27 * 10 ** -6},
+                {'name': 'Substrate attenuation', 'type': 'float', 'value': 200 * 10 ** -6}]}])
+        self.materialstree.setParameters(self.materialparam, showTop=False)
+
+        from .trapezoidparameter import TrapezoidAnglesWidgetParameter
+        trap = TrapezoidAnglesWidgetParameter(name='Test Trapezoids')
+        self.datatreatmentparam.addChild(trap)
+
+        self.datatreatmentparam.param('Experimental input', 'Data treatment').sigActivated.connect(self.datatreatment)
+        self.datatreatmentparam.param('Initial line Profile', 'Simulation').sigActivated.connect(self.fit)
+
+        super(plugin, self).__init__(*args, **kwargs)
+
+    def update_model(self, widget):
+        guiinvoker.invoke_in_main_thread(self.bottomwidget.plotLineProfile, *widget.modelParameters)
+
+    def update_right_widget(self):
+        guiinvoker.invoke_in_main_thread(self.datatreatmentparam.param('Fit output', 'num trap fit').setValue,
+                                         self.Num_trap)
+        guiinvoker.invoke_in_main_thread(self.datatreatmentparam.param('Fit output', 'H fit').setValue, self.H)
+        guiinvoker.invoke_in_main_thread(self.datatreatmentparam.param('Fit output', 'w0 fit').setValue, self.w0)
+        guiinvoker.invoke_in_main_thread(self.datatreatmentparam.param('Fit output', 'Beta fit').setValue, self.Beta1)
+        guiinvoker.invoke_in_main_thread(self.datatreatmentparam.param('Fit output', 'f_val').setValue, self.fval)
+
+    def openfiles(self, files, operation=None, operationname=None):
+        self.activate()
+        if type(files) is not list:
+            files = [files]
+
+        widget = widgets.OOMTabItem(itemclass=CDSAXSWidget, src=files, operation=operation, operationname=operationname,
+                                    plotwidget=self.bottomwidget, toolbar=self.toolbar)
+        self.centerwidget.addTab(widget, os.path.basename(files[0]))
+        self.centerwidget.setCurrentWidget(widget)
+
+        fitrunnable = threads.RunnableMethod(data_treatment.readheader, method_args=(files,),
+                                             callback_slot=self.update_experimental_input)
+        threads.add_to_queue(fitrunnable)
+
+    def update_experimental_input(self, files, angle_start, angle_end, angle_step):
+        guiinvoker.invoke_in_main_thread(self.datatreatmentparam.param('Experimental input', 'Start angle').setValue,
+                                         angle_start)
+        guiinvoker.invoke_in_main_thread(self.datatreatmentparam.param('Experimental input', 'End angle').setValue,
+                                         angle_end)
+        guiinvoker.invoke_in_main_thread(self.datatreatmentparam.param('Experimental input', 'Angle step').setValue,
+                                         angle_step)
+        self.files = files
+
+    def datatreatment(self):
+        Phi_min, Phi_max, Phi_step, Pitch = self.datatreatmentparam['Experimental input', 'Start angle'], \
+                                            self.datatreatmentparam['Experimental input', 'End angle'], \
+                                            self.datatreatmentparam[
+                                                'Experimental input', 'Angle step'], self.datatreatmentparam[
+                                                'Experimental input', 'Line pitch']
+        substratethickness, substrateattenuation = self.materialparam['Material', 'Substrate thickness'], \
+                                                   self.materialparam['Material', 'Substrate attenuation']
+        fitrunnable = threads.RunnableMethod(data_treatment.loadRAW, method_args=(
+        self.files, Phi_min, Phi_max, Phi_step, Pitch, substratethickness, substrateattenuation),
+                                             callback_slot=self.diplay_experimentaldata)
+        threads.add_to_queue(fitrunnable)
+
+    def diplay_experimentaldata(self, data, img, qx, qz, I):
+        self.qx = qx
+        self.qz = qz
+        self.I = I
+        self.getCurrentTab().CDRawWidget.setImage(data)
+        self.getCurrentTab().CDCartoWidget.setImage(img)
+        self.getCurrentTab().update_profile_ini(qz, I)
+
+    def fit(self):
+        activeSet = self.getCurrentTab()
+        activeSet.setCurrentWidget(activeSet.CDModelWidget)
+        self.DW, self.I0, self.Bkg = 0.11, 3.0, 1  # default
+        self.H, self.w0, self.Beta1, self.Num_trap = self.datatreatmentparam['Initial line Profile', 'Heigth'], \
+                                                     self.datatreatmentparam['Initial line Profile', 'Linewidth'], \
+                                                     self.datatreatmentparam['Initial line Profile', 'Sidewall angle'], \
+                                                     self.datatreatmentparam['Initial line Profile', 'Number trapezoid']
+
+        self.best_param = [self.DW, self.I0, self.Bkg, self.H, self.w0] + [int(self.Beta1) for i in
+                                                                           range(0, self.Num_trap, 1)]
+        self.Ifit = data_treatment.SL_model1(self.qx, self.qz, self.best_param)
+        self.diplay_fitteddata(self.Ifit, self.best_param)
+
+        fitrunnable1 = threads.RunnableMethod(data_treatment.fitting_cmaes, method_args=(
+        self.qx, self.qz, self.I, self.H, self.w0, self.Beta1, self.Num_trap), callback_slot=self.diplay_fitteddata)
+        # fitrunnable2 = threads.RunnableMethod(data_treatment.fitting_mcmc, method_args=(self.qx, self.qz, self.I, self.H, self.w0, self.Beta1, self.Num_trap))
+        threads.add_to_queue(fitrunnable1)
+        self.update_right_widget()
+        # threads.add_to_queue(fitrunnable2)
+
+    def diplay_fitteddata(self, Ifit, best_param, fval=None):
+        if fval is None:
+            self.fval = 0
+        else:
+            self.Ifit = Ifit
+            self.best_param = [best_param[0], best_param[1], best_param[2], best_param[3], best_param[4]] + [
+                best_param[i] for i in range(5, len(best_param), 1)]
+            self.fval = fval
+
+        self.update_right_widget()
+        self.bottomwidget.plotLineProfile(self.best_param[3], self.best_param[4], np.array(self.best_param[5:]))
+        self.getCurrentTab().update_profile(self.qz, self.I, self.Ifit)
+
+    def currentChanged(self, index):
+        for tab in [self.centerwidget.widget(i) for i in range(self.centerwidget.count())]:
+            tab.unload()
+        self.centerwidget.currentWidget().load()
+        self.getCurrentTab().sigDrawModel.connect(self.update_model)
+        self.getCurrentTab().sigDrawParam.connect(self.update_right_widget)
+        self.getCurrentTab().sigDrawParam.connect(self.update_experimental_input)
+
+    def tabClose(self, index):
+        self.centerwidget.widget(index).deleteLater()
+
+    def getCurrentTab(self):
+        if self.centerwidget.currentWidget() is None: return None
+        if not hasattr(self.centerwidget.currentWidget(), 'widget'): return None
+        return self.centerwidget.currentWidget().widget
+
+
+class CDSAXSWidget(QtGui.QTabWidget):
+    sigDrawModel = QtCore.Signal(object)
+    sigDrawParam = QtCore.Signal(object)
+
+    def __init__(self, src, *args, **kwargs):
+        super(CDSAXSWidget, self).__init__()
+
+        self.CDRawWidget = custom_widget.CDRawWidget()
+        self.CDCartoWidget = custom_widget.CDCartoWidget()
+        self.CDModelWidget = custom_widget.CDModelWidget()
+
+        self.addTab(self.CDRawWidget, 'RAW')
+        self.addTab(self.CDCartoWidget, 'Cartography')
+        self.addTab(self.CDModelWidget, 'Model')
+
+        self.setTabPosition(self.South)
+        self.setTabShape(self.Triangular)
+
+    def update_model(self):
+        self.sigDrawModel.emit(self)
+
+    def update_right_widget(self):
+        self.sigDrawParam.emit(self)
+
+    @property
+    def modelParameters(self):
+        # h,w,langle,rangle=None
+        # 3,4,...
+        return self.best_corr[3], self.best_corr[4], self.best_corr[5:]
+
+    def update_profile_ini(self, qz, I):
+        """
+        Display the experimental peak intensity rescaled
+        """
+        for order in range(0, len(I), 1):
+            I[order] -= min(I[order])
+            I[order] /= max(I[order])
+            I[order] += order + 1
+
+            guiinvoker.invoke_in_main_thread(self.CDModelWidget.orders[order].setData, qz[order], np.log(I[order]))
+
+    def update_profile(self, qz, I, I_fit):
+        """
+        Display the simulated/experimental peak intensity rescaled
+        """
+        for order in range(0, len(I), 1):
+            I[order] -= min(I[order])
+            I[order] /= max(I[order])
+            I[order] += order + 1
+
+            I_fit[order] -= min(I_fit[order])
+            I_fit[order] /= max(I_fit[order])
+            I_fit[order] += order + 1
+
+            guiinvoker.invoke_in_main_thread(self.CDModelWidget.orders[order].setData, qz[order], np.log(I[order]))
+            guiinvoker.invoke_in_main_thread(self.CDModelWidget.orders1[order].setData, qz[order], np.log(I_fit[order]))
---
+++ xicam/CDSAXS/cdsaxs.py
@@ -0,0 +1,279 @@
+# --coding: utf-8 --
+import numpy as np
+from astropy.modeling import models, fitting
+from pyFAI import AzimuthalIntegrator, detectors
+from scipy.ndimage import filters
+from xicam import config
+from pipeline import loader
+import warnings
+
+
+def configu(beamline, phi, file):
+    """ Garbage function which allow to read the different images from several beamlines (CMS, 733, 11012 .....)
+    It allow to read the I0, sample_rotation and correct the noise level => different detector and header (different name and level of noise)
+
+    Parameters
+    ----------
+    a (string) : Name of the beamline
+    file (list of string) : name of the file
+    phi (float32) : sample_rotation in radians => since some header do not contained this value, this one corresponds to the one entered by the user
+
+    Returns
+    -------
+    phi (float32) : sample_rotation in radians (if the angle is contained in the header, the value is overwritten)
+    data (array of float32): 2D scattering image
+    img1 (array of float32): 2D scattering image rotated, displayed as the raw data
+    """
+    if beamline == '733':
+        data = np.flipud(loader.loadimage(file))
+        img1 = np.rot90(loader.loadimage(file), 1)
+        I_0 = np.float(loader.loadparas(file)['Izero'])
+        phi = np.deg2rad(np.float(loader.loadparas(file)['Sample Rotation Stage']))
+        # data = data / np.abs(I_0)
+    elif beamline == '1102':
+        data = np.rot90(np.flipud(loader.loadimage(file)), 1)  # Flipud works for RSOXS data
+        img1 = loader.loadimage(file)
+        I_0 = loader.loadparas(file)['AI 3 Izero']
+        th_sc, th_0 = 0.984, -2.14
+        phi = np.deg2rad(th_sc * (loader.loadparas(file)['sample theta'] - th_0) - 90)
+        t = loader.loadparas(file)['EXPOSURE']
+        data = data / np.abs(I_0 * t)
+        data = data / 1.
+        data -= np.mean(data[: -5, :])  # Remove background
+        data[np.where(data < 0)] = 0.000001
+    else:
+        data = np.flipud(loader.loadimage(file))
+        img1 = np.rot90(loader.loadimage(file), 1)
+    return phi, data, img1
+
+
+def test(substratethickness, substrateattenuation, Pitch, q_pitch, or_hor, header_dic):
+    """ This function is used to manage all the process of extraction from the name of the file (it is not very usefull but was easier to use for multiprocessing). It is just calling the different functions.
+
+    Parameters
+    ----------
+    substratethickness, substrateattenuation, Pitch (float32): substrate thickness, substrate attenuation, pitch
+    Data (list of float32 : ([], [])) : Data contains the list of file sected by the user, as well as the sample rotation values attributed by the User
+
+    Returns
+    -------
+    I_cor, q_x, q_z (list of float32) : List of all the I/qx/qz values from the 1D profile => used for displaying the carthography
+    img1 (2D array of float32) : 2D scattering image => displayed as the raw data
+    Qxexp, Q__Z, I_peaks (list of float32) : List of all the qx/qz/I values of peaks => Use as the experimental qx for fitting
+
+    """
+    try:
+        beamline = 'CMS'
+        phi, data, img1 = configu(beamline, header_dic[0], header_dic[1])
+        q_n, q_x, q_z, I, wavelength = reduceto1dprofile(data, phi, beamline)
+        I_cor = correc_Iexp(I, substratethickness, substrateattenuation, header_dic[0])
+        Qxexp, Q__Z, I_peaks = Find_peak(q_n, I_cor, Pitch, header_dic[0], wavelength, q_pitch)
+        return I_cor, img1, q_x, q_z, Qxexp, Q__Z, I_peaks
+
+    except IndexError:
+        print
+        'Error in {}'.format(file)
+
+
+def image_orientation(file):
+    data = np.flipud(loader.loadimage(file))
+    mask = config.activeExperiment.getDetector().calc_mask()
+
+    AI = AzimuthalIntegrator()
+    AI = config.activeExperiment.getAI()
+    centerX, directDist, centerY = AI.getFit2D()['centerX'], AI.getFit2D()["directDist"], AI.getFit2D()["centerY"]
+    AI.setFit2D(directDist, -25, 21)
+
+    cake, q, chi = AI.integrate2d(data[int(centerY - 20): int(centerY + 20), int(centerX + 25):],
+                                  config.settings['Integration Bins (q)'], config.settings['Integration Bins (χ)'])
+    cakemask, q, chi = AI.integrate2d(np.ones_like(data[int(centerY - 20): int(centerY + 20), int(centerX + 25):]),
+                                      config.settings['Integration Bins (q)'], config.settings['Integration Bins (χ)'])
+    maskedcake = np.ma.masked_array(cake, mask=cakemask <= 0)
+    chiprofile = np.ma.average(maskedcake, axis=1)
+
+    kernel = np.zeros_like(chiprofile)
+    kernel[0] = 1
+    kernel[len(kernel) / 2] = 1
+
+    tiltprofile = filters.convolve1d(kernel, chiprofile, mode='wrap')
+    tilt = tiltprofile.argmax() / float(config.settings['Integration Bins (χ)']) * 360.
+
+    return tilt
+
+
+def reduceto1dprofile(data, phi, beamline):
+    """ First, q indices are calculated from the 2d image (pixel number). Then a correction of the sample tilt is done (if necessary) through the caking of pyFai. Finally, only few pixels around the scattering peaks coming from the gratings are selected and summed.
+
+    Parameters
+    ----------
+    data (array of float32): 2D scattering image
+    phi (float32) : sample_rotation in radians
+
+    Returns
+    -------
+    q_n (list of float32) : List of all the q values of the extracted 1D profile
+    q_x, q_z, intensity (lists of float32) : List of all the qx/qz/intensity values of the extracted 1D profile => used for displaying the carthography
+    wavelength (float32) : wavelength
+
+    """
+
+    # cosmic masking
+    if beamline == '1102':
+        mask = config.activeExperiment.getDetector().calc_mask()
+    elif beamline == '733':
+        mask = (data > np.percentile(data, 98))
+        mask = mask + config.activeExperiment.getDetector().calc_mask()
+    else:
+        # mask = (data > np.percentile(data, 98))
+        mask = config.activeExperiment.getDetector().calc_mask()
+
+    AI = AzimuthalIntegrator()
+    AI = config.activeExperiment.getAI()
+    centerX, directDist, centerY = AI.getFit2D()['centerX'], AI.getFit2D()["directDist"], AI.getFit2D()["centerY"]
+    AI.setFit2D(directDist, -25, 21)
+
+    wavelength = config.activeExperiment.getvalue('Wavelength')
+
+    cake, q, chi = AI.integrate2d(data[int(centerY - 20): int(centerY + 20), int(centerX + 25):],
+                                  config.settings['Integration Bins (q)'], config.settings['Integration Bins (χ)'])
+    cakemask, q, chi = AI.integrate2d(np.ones_like(data[int(centerY - 20): int(centerY + 20), int(centerX + 25):]),
+                                      config.settings['Integration Bins (q)'], config.settings['Integration Bins (χ)'])
+    maskedcake = np.ma.masked_array(cake, mask=cakemask <= 0)
+    chiprofile = np.ma.average(maskedcake, axis=1)
+
+    kernel = np.zeros_like(chiprofile)
+    kernel[0] = 1
+    kernel[len(kernel) / 2] = 1
+
+    tiltprofile = filters.convolve1d(kernel, chiprofile, mode='wrap')
+
+    if beamline == '1102':
+        tilt = tiltprofile[0:200].argmax() / float(config.settings['Integration Bins (χ)']) * 360.
+        tilt = 0
+    else:
+        tilt = tiltprofile.argmax() / float(config.settings['Integration Bins (χ)']) * 360.
+        # print(tilt)
+
+    # correct tilt
+    AI.set_rot3(np.deg2rad(tilt))
+    AI.setFit2D(directDist, min(0, centerX), 21)
+    cake, q, chi = AI.integrate2d(data[int(centerY - 20): int(centerY + 20), int(max(0, centerX)):],
+                                  config.settings['Integration Bins (q)'], 100)
+    cakemask, q, chi = AI.integrate2d(np.ones_like(data[int(centerY - 20): int(centerY + 20), int(max(0, centerX)):]),
+                                      config.settings['Integration Bins (q)'], 100)
+
+    slc = np.vstack(cake[45:55, :])  # for other side, index around 500
+    profile1 = np.sum(slc, axis=0)
+
+    q_n = np.array([q[val] for val in np.where(profile1 > 0)[0]])
+    q_x = np.array(q_n * np.cos(phi + 2 * np.arcsin(q_n * wavelength / (4. * np.pi))))
+    q_z = np.array(q_n * np.sin(phi + 2 * np.arcsin(q_n * wavelength / (4. * np.pi))))
+    intensity = np.array([val for val in profile1 if val > 0])
+
+    return q_n, q_x, q_z, intensity, wavelength
+
+
+def Find_peak(q, profiles, pitch, phi, wavelength, q_pitch):
+    """ Find the peak position and intensity by fitting gaussians
+
+    Parameters
+    ----------
+    q, profiles (list of float32) : List of all the q/intensity values of the 1D profile
+    pitch, phi, wavelength (float32) : pitch, sample_rotation, wavelength
+
+    Returns
+    -------
+    Qxexp, Q__Z, I_peaks(lists of float32) : List of all the qx/qz/I values of peaks => Use as the experimental qx/qz/I for fitting
+
+
+    """
+    Qxexp, Q__Z, I_peaks = [[]] * 20, [[]] * 20, [[]] * 20
+    q_pitch /= (1. * np.cos(phi) + 0.0000001)
+    ind_pi = np.argmin(np.abs(q - q_pitch))
+
+    j, q_ref, nb = 0, 0, 0
+    while abs((j + 1) * ind_pi) < len(profiles) - 1:
+        ind = (j + 1) * ind_pi
+        # Switch to masked array
+        pos_gauss2 = np.linspace(max(ind - 25, 0), min(ind + 35, len(profiles) - 1),
+                                 int(1 + min(ind + 35, len(profiles) - 1) - (max(ind - 25, 0))), dtype=np.int32)
+        g_init2 = models.Gaussian1D(amplitude=profiles[pos_gauss2].max(), mean=q[ind], stddev=0.01)
+        fit_g = fitting.LevMarLSQFitter()
+        with warnings.catch_warnings(record=True) as w:
+            # Cause all warnings to always be triggered.
+            warnings.simplefilter("always")
+            # Trigger a warning.
+            gg = fit_g(g_init2, q[pos_gauss2], profiles[pos_gauss2])
+            I_peaks[j] = I_peaks[j] + [np.float(
+                gg.amplitude.value if (not (w) and not (fit_g.fit_info['param_cov'] is None)) else profiles[
+                    pos_gauss2].max())]
+
+            if (not (w) and not (
+                    fit_g.fit_info['param_cov'] is None and gg.mean.value < q[pos_gauss2[0]] and gg.mean.value > q[
+                pos_gauss2[-1]])):
+                ind_pi = np.int(np.argmin(np.abs(q - gg.mean.value)) / (j + 1))
+        j += 1
+
+    q_ref = q_pitch
+    for j in range(0, len(filter(None, I_peaks)), 1):
+        Qxexp[j] = Qxexp[j] + [
+            (j + 1) * q_ref * np.cos(phi + 2 * np.arcsin((j + 1) * q_ref * wavelength / (4. * np.pi)))]
+        Q__Z[j] = Q__Z[j] + [(j + 1) * q_ref * np.sin(phi + 2 * np.arcsin((j + 1) * q_ref * wavelength / (4. * np.pi)))]
+
+    return Qxexp, Q__Z, I_peaks
+
+
+# Correction of the footprint and substrate attenuation // Addition of sample size/sample attenuation and polarization
+def correc_Iexp(pr, substratethickness, substrateattenuation, phi):
+    """ Correction of the intensity on 1d experimental profiles (beam footprint, absorption by the substrate, sample size and beamsize)
+
+    Parameters
+    ----------
+    pr (list of float32): List of all the intensity values of the extracted 1D profile
+    substratethickness, substrateattenuation, phi (float32): substrate thickness, substrate attenuation, sample rotation
+
+    Returns
+    -------
+    pr (list of float32): List of all the intensity values corrected
+    """
+    footprintcorr = 'True'
+    abscorr = 'True'
+    samplesizecorr = 'False'
+    fwhm, sample_size = 1, 1
+    for i in range(0, len(pr), 1):
+        footprintfactor = np.abs(np.cos(phi)) if footprintcorr else 1
+        absfactor = np.exp(
+            -substratethickness * substrateattenuation * (1 - 1 / np.cos(phi + 0.000000001))) if abscorr else 1
+        pr[i] *= absfactor * footprintfactor
+    return pr
+
+
+def interpolation(q_x, q_z, I_i, sampling_size=(400, 400)):
+    """ interpolation of the (qx,qz) carthography. Conversion from 3 arrays (q_x as x, q_z as y and I the intensity at q_x, q_z) to display the cartography
+
+    Parameters
+    ----------
+    q_x, q_z, I_i (arrays of float32): 1D array of all the qx/qz/I values
+    sampling_size (float32): Dimension of the carthography
+
+    Returns
+    -------
+    img(2D array of float 32): 2D carthography
+    """
+    roi_size = 400
+    img = np.zeros((roi_size, roi_size))
+
+    qj = np.floor(((q_x - q_x.min()) / (q_x - q_x.min()).max()) * (sampling_size[0] - 1)).astype(np.int32)
+    qk = np.floor(((q_z - q_z.min()) / (q_z - q_z.min()).max()) * (sampling_size[1] - 1)).astype(np.int32)
+    I = I_i
+
+    qj_shifted = qj - qj.min()
+    qk_shifted = qk - qk.min()
+
+    Isel = I
+    for i in range(0, len(Isel), 1):
+        img[qj_shifted[i], qk_shifted[i]] += Isel[i]
+
+    log_possible = np.where(img != 'nan')
+    img[log_possible] = np.log(img[log_possible] - img[log_possible].min() + 1.)
+    return img
